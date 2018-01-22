6001cb5182c8a8e16b6de1fd0b97e44e8772b7b7
Gibrid89 <34302211+Gibrid89@users.noreply.github.com>
2018-01-22 20:36:22 +0300

    Initial commit

---
+++ UnitTestConsoleCalc/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestConsoleCalc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UnitTestConsoleCalc")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b35ef43d-fa93-4073-965e-4c2975d19413")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestConsoleCalc/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleCalc;
+
+namespace UnitTestConsoleCalc
+{
+    [TestClass]
+    public class UnitTestHelper
+    {
+        [TestMethod]
+        public void ConvertToReversePolishNotationTest()
+        {
+            var calc = new CalcString();
+            calc.Operators.Add(new Operator('^', 3));
+
+            var result = calc.ConvertToReversePolishNotation("1+1-2");
+            var answer = new string[] { "1", "1", "+", "2", "-" };
+            for (int i = 0; i < answer.Length; i++)
+            {
+                Assert.AreEqual(result[i], answer[i]);
+            }
+
+            result = calc.ConvertToReversePolishNotation("2+4*2/(1-5)^2");
+            answer = new string[] { "2", "4", "2", "*", "1", "5", "-", "2", "^", "/", "+" };
+            for (int i = 0; i < answer.Length; i++)
+            {
+                Assert.AreEqual(result[i], answer[i]);
+            }
+
+            result = calc.ConvertToReversePolishNotation("3^2+6/2*(5+5)");
+            answer = new string[] { "3", "2", "^", "6", "2", "/", "5", "5", "+", "*", "+" };
+            for (int i = 0; i < answer.Length; i++)
+            {
+                Assert.AreEqual(result[i], answer[i]);
+            }
+        }
+
+    }
+}
